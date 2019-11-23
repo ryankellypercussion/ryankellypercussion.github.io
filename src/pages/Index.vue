dfa8aa7842b546b180f08b4331c6c3cad56f6913
Ryan Kelly <ryan@ryankellypercussion.com>
2019-11-23 12:01:36 -0500

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+
+    <h1>Coming Soon!</h1>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Coming Soon!'
+  }
+}
+</script>
+
+<style>
+</style>
